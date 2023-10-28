81e2326b19a74ec8c895847ddefc917fe84374d7
Timimah <44163985+Timimah@users.noreply.github.com>
2023-10-28 12:49:43 +0100

    fixed mobile nav

--- AidFusion/src/components/Navbar.jsx
+++ AidFusion/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -12,9 +12,23 @@ import {
 import { Button } from './ui/button'
 
 export const Navbar = () => {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggle = () => {
+        setIsToggled(!isToggled);
+        if (isToggled) {
+            // Logic when the toggle is ON
+            console.log('Toggled ON');
+            // Perform actions for ON state
+        } else {
+            // Logic when the toggle is OFF
+            console.log('Toggled OFF');
+            // Perform actions for OFF state
+        }
+    };
     return (
         <>
-        {/* //medium screens and above */}
+            {/* //medium screens and above */}
             <div className='py-4 px-5 gap-5 justify-between items-center hidden md:flex'>
                 <div className='text-xl'>AidFusion</div>
                 <div className='w-1/2'>
@@ -29,22 +43,32 @@ export const Navbar = () => {
                 </div>
             </div>
             {/* //small screens and below */}
-            <div className='py-4 px-5 gap-5 justify-between items-center flex flex-col md:flex-row'>
-      <div className='text-xl text-center mb-4 md:mb-0'>AidFusion</div>
-      <div className='w-full md:w-auto flex justify-center mb-4 md:mb-0'>
-        <Button variant="outline" className="bg-accent border-0">Toggle</Button>
-      </div>
-      <div className='text-center md:text-left md:w-1/2'>
-        <ul className='flex flex-col md:flex-row md:justify-around md:items-center'>
-          <li>About Us</li>
-          <li>How it Works</li>
-          <li>Contact us</li>
-        </ul>
-      </div>
-      <div className='text-center md:text-right w-full md:w-auto mt-4 md:mt-0'>
-        <Button variant="outline" className="bg-accent border-0">Sign Up</Button>
-      </div>
-    </div>
+            <div className='py-4 px-5 gap-5 justify-between items-center flex md:hidden'>
+                <div className='flex'>
+                    <Button
+                        variant="outline"
+                        className={`bg-accent border-0 ${isToggled ? 'bg-green-500' : 'bg-red-500'}`}
+                        onClick={toggle}
+                    >
+                        {isToggled ? 'Open' : 'Close'}
+                    </Button>
+                </div>
+
+                {isToggled && (
+                    <div className='text-center md:text-left md:w-1/2'>
+                        <ul className='flex flex-col md:flex-row md:justify-around md:items-center'>
+                            <li>About Us</li>
+                            <li>How it Works</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                )}
+                <div className='text-xl text-center'>AidFusion</div>
+
+                <div className='text-center'>
+                    <Button variant="outline" className="bg-accent border-0">Sign Up</Button>
+                </div>
+            </div>
         </>
     )
 }
