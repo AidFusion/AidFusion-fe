6a9957853fd64947f527dc56d64dc500a51741d2
Timimah <44163985+Timimah@users.noreply.github.com>
2023-10-28 15:52:57 +0100

    update footer

--- AidFusion/src/components/Footer.jsx
+++ AidFusion/src/components/Footer.jsx
@@ -6,36 +6,33 @@ export const Footer = () => {
     <div className='p-10 flex flex-col mx-auto md:flex-row md:gap-10 gap-10 md:space-x-20 '>
       <div className='flex justify-evenly gap-10 md:gap-14'>
         <div className='flex flex-col gap-2'>
-          <div className='font-semibold text-xl'>About us</div>
+          <div className='font-semibold text-xl'>Mobile App</div>
           <ul className=''>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Download the App</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
-            </li>
-            <li>
-              <Link to="">Our Team</Link>
-            </li>
-            <li>
-              <Link to="">Our Team</Link>
+              <Link to="">App features</Link>
             </li>
           </ul>
         </div>
         <div className='flex flex-col gap-2'>
-          <div className='font-semibold text-xl'>About us</div>
+          <div className='font-semibold text-xl'>Privacy & Terms</div>
           <ul className=''>
-            <li>
-              <Link to="">Our Team</Link>
+          <li>
+              <Link to="">Privacy Policy</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Terms odf Service</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Cookies Policy</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Data Protection</Link>
+            </li>
+            <li>
+              <Link to="">Code of Conduct</Link>
             </li>
           </ul>
         </div>
@@ -48,30 +45,33 @@ export const Footer = () => {
               <Link to="">Our Team</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Contact Us</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Blog & News</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Testimonials</Link>
             </li>
           </ul>
         </div>
         <div className='flex flex-col gap-2'>
-          <div className='font-semibold text-xl'>About us</div>
+          <div className='font-semibold text-xl'>Support & Resources</div>
           <ul className=''>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Help Center</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Donate</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Volunteer</Link>
             </li>
             <li>
-              <Link to="">Our Team</Link>
+              <Link to="">Partner with Us</Link>
+            </li>
+            <li>
+              <Link to="">Resources for Restaurants</Link>
             </li>
           </ul>
         </div>
